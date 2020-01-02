e7e99f0c563708c5838d3ba4af20ba688d7bda62
Samuel Vienne <samuelvienne@yahoo.fr>
2020-01-02 16:00:37 -0500

    js final

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const btnPlay = document.querySelector('#btnPlay');
 const btnStop = document.querySelector('#btnStop');
 const videoDuration = player.duration;
 const speed = document.querySelector('#speed');
-
 const seekbar = document.querySelector('#seekbar');
 
-btnPlay.addEventListener('click', playVideo);
-btnStop.addEventListener('click', stopVideo);
-speedSlider.addEventListener('change', adjustSpeed);
+/* functions */
 
 function playVideo(evt){
 	if(player.paused){
@@ -32,3 +29,24 @@ function adjustSpeed(evt){
 	speed.innerHTML = `${currentPlaybackRate}X`;
 	player.playbackRate = currentPlaybackRate;
 }
+
+function setupSeekbar(){
+	seekbar.max = player.duration;
+}
+
+function seekVideo(){
+	player.currentTime = seekbar.value; /* same as evt.target.value */
+}
+
+function updateUI(){
+	seekbar.value = video.currentTime;
+}
+
+/* events */
+
+btnPlay.addEventListener('click', playVideo);
+btnStop.addEventListener('click', stopVideo);
+speedSlider.addEventListener('change', adjustSpeed);
+player.addEventListener('durationchange', setupSeekbar);
+seekbar.addEventListener('change', seekVideo);
+seekbar.addEventListener('timeupdate', updateUI);
